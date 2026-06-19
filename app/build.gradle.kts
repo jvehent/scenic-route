c80dba56fa77a4308aac8837bbd8e9a6b1ee766c
Julien Vehent <julien@vehent.org>
2026-06-19 10:52:19 -0400

    release 0.1.9 (code 10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "com.senikroute"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.1.7"
+        versionCode = 10
+        versionName = "0.1.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
